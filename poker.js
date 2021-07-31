77bd1b7d30554ebfd5262891749bb114252f1991
Nathan Reidy <nathanjreidy@gmail.com>
2021-07-31 13:01:14 +1000

    added function for indentical suit checking

--- poker.js
+++ poker.js
@@ -16,8 +16,9 @@ const uniqueCards = [
 const suits = ["D", "H", "S", "C"];
 const allCards = [];
 const handWinCount = [{ playerOne: null }, { playerTwo: null }];
+const royalFlush = ["10"];
 
-// Creates data for all possible card combinations
+// Creates data for all possible card combinations and stores it in allCards
 const createAllCards = (uniqueCards, suits) => {
   uniqueCards.map((card) => {
     suits.map((suit) => {
@@ -26,16 +27,30 @@ const createAllCards = (uniqueCards, suits) => {
   });
 };
 
-createAllCards(uniqueCards, suits);
-console.log(allCards);
-
 const createHands = () => {
   return;
 };
 
+// Checks if the player has the same suits for each card
+const checkSuitIdentical = (playerCards) => {
+  for (let i = 0; i < playerCards.length - 1; i++) {
+    if (
+      playerCards[i][playerCards[i].length - 1] !=
+      playerCards[i + 1][playerCards[i + 1].length - 1]
+    ) {
+      return "Suits are different";
+    }
+  }
+
+  return "Suits are identical";
+};
+
 const checkResults = (playerOneCards, playerTwoCards) => {
-  // playerOneCards and playerTwoCards are arrays of 5 randomly selected cards each
+  // playerOneCards and playerTwoCards are each an array of 5 randomly selected cards
   // check results of player one's cards
   // check results of player two's cards
   // check winner and add one to handWinCount for the winner
 };
+
+createAllCards(uniqueCards, suits);
+console.log(checkRoyalFlush(["2E", "3E", "8E", "9E", "10E"]));
