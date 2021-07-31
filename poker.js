443409213daa6427e873cade9826c6210f8c385b
Nathan Reidy <nathanjreidy@gmail.com>
2021-07-31 16:57:31 +1000

    Added function for checking frequency of card (count)

--- poker.js
+++ poker.js
@@ -45,7 +45,8 @@ const checkSuitIdentical = (playerCards) => {
   return "Suits are identical";
 };
 
-const straight = (playerCards) => {
+// Checks if the player has a straight
+const checkStraight = (playerCards) => {
   // We want to find each card in our uniqueCards array and see if their indexes are consecutive
   const cardIndexInUniqueCards = [];
   playerCards.forEach((card, index) => {
@@ -74,6 +75,55 @@ const straight = (playerCards) => {
   return "We have a straight!";
 };
 
+// Check for a pair by adding unique values to a new array called pairValues
+const checkPair = (playerCards) => {
+  // We want to find each card in our uniqueCards array and see if their indexes are consecutive
+  const cardIndexInUniqueCards = [];
+  let pairValues = [];
+  playerCards.forEach((card, index) => {
+    // Match the specific card to its index in unique cards
+    if (!card.includes("10")) {
+      cardIndexInUniqueCards.push(uniqueCards.indexOf(card[0]));
+    } else {
+      cardIndexInUniqueCards.push(uniqueCards.indexOf(card.substr(0, 2)));
+    }
+  });
+  // Sort items in array in ascending order
+  cardIndexInUniqueCards.sort((a, b) => a - b);
+
+  // If the difference between the items in the array is 0, for every item,
+  // then we have a duplicate
+  for (let i = 0; i < cardIndexInUniqueCards.length - 1; i++) {
+    let newArr = pairValues.filter(
+      (obj) => obj.card == uniqueCards[cardIndexInUniqueCards[i]]
+    );
+
+    if (
+      parseInt(cardIndexInUniqueCards[i + 1]) -
+        parseInt(cardIndexInUniqueCards[i]) ==
+        0 &&
+      newArr.length == 0
+    ) {
+      pairValues.push({
+        card: uniqueCards[cardIndexInUniqueCards[i]],
+        count: 2,
+      });
+    }
+
+    if (
+      parseInt(cardIndexInUniqueCards[i + 1]) -
+        parseInt(cardIndexInUniqueCards[i]) ==
+        0 &&
+      newArr.length !== 0
+    ) {
+      newArr[0].count += 1;
+
+      pairValues = [...pairValues];
+    }
+  }
+  return pairValues;
+};
+
 const checkResults = (playerOneCards, playerTwoCards) => {
   // playerOneCards and playerTwoCards are each an array of 5 randomly selected cards
   // check results of player one's cards
@@ -83,4 +133,5 @@ const checkResults = (playerOneCards, playerTwoCards) => {
 
 createAllCards(uniqueCards, suits);
 // console.log(checkRoyalFlush(["2E", "3E", "8E", "9E", "10E"]));
-console.log(straight(["2S", "9D", "6D", "4H", "5C"]));
+// console.log(checkStraight(["9S", "8D", "QD", "10H", "JC"]));
+console.log(checkPair(["8S", "7D", "7D", "10H", "10C"]));
