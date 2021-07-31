5a15aac85f2d4b5a9f109850f09acf826b15d1cd
Nathan Reidy <nathanjreidy@gmail.com>
2021-07-31 14:13:14 +1000

    Wrote function to check for straight

--- poker.js
+++ poker.js
@@ -45,6 +45,35 @@ const checkSuitIdentical = (playerCards) => {
   return "Suits are identical";
 };
 
+const straight = (playerCards) => {
+  // We want to find each card in our uniqueCards array and see if their indexes are consecutive
+  const cardIndexInUniqueCards = [];
+  playerCards.forEach((card, index) => {
+    // Match the specific card to its index in unique cards
+    if (!card.includes("10")) {
+      cardIndexInUniqueCards.push(uniqueCards.indexOf(card[0]));
+    } else {
+      cardIndexInUniqueCards.push(uniqueCards.indexOf(card.substr(0, 2)));
+    }
+  });
+  // Sort items in array in ascending order
+  cardIndexInUniqueCards.sort((a, b) => a - b);
+
+  // If the difference between the items in the array is 1, for every item,
+  // then we have a straight
+  for (let i = 0; i < cardIndexInUniqueCards.length - 1; i++) {
+    if (
+      parseInt(cardIndexInUniqueCards[i + 1]) -
+        parseInt(cardIndexInUniqueCards[i]) !==
+      1
+    ) {
+      return "Not a straight!";
+    }
+  }
+
+  return "We have a straight!";
+};
+
 const checkResults = (playerOneCards, playerTwoCards) => {
   // playerOneCards and playerTwoCards are each an array of 5 randomly selected cards
   // check results of player one's cards
@@ -53,4 +82,5 @@ const checkResults = (playerOneCards, playerTwoCards) => {
 };
 
 createAllCards(uniqueCards, suits);
-console.log(checkRoyalFlush(["2E", "3E", "8E", "9E", "10E"]));
+// console.log(checkRoyalFlush(["2E", "3E", "8E", "9E", "10E"]));
+console.log(straight(["2S", "9D", "6D", "4H", "5C"]));
