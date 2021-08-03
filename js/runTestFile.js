717a49ae19af531ec7bfb583c38f6c7b67f19c76
Nathan Reidy <nathanjreidy@gmail.com>
2021-08-03 23:08:32 +1000

    Cleaned code comments

--- js/runTestFile.js
+++ js/runTestFile.js
@@ -1,4 +1,4 @@
 const { array, playGame } = require("./checkGameResults.js");
 
-// Runs the entire test file of poker games and returns the expected winner
+// Runs the entire test file of poker games and returns the expected output
 playGame(array.length, (runTest = true));
